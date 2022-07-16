c7cc68eb48e38f4c314a4fbe717941befa420329
Dmitry Marov <d.marov94@gmail.com>
2022-07-16 13:09:10 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Nodesocket Server
 
 ## Example of typescript setup for nodejs server
+
+## Development
+
+- `npm run lint` - to lint code
+- `npm run lint:fix` - to fix lint errors
+- `npm run dev` - to start development server
+- `npm run build` - to build for production
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
-import { Server } from 'socket.io'
-import http from 'http'
+import { Server } from "socket.io";
+import http from "http";
 
-const server = http.createServer()
+const server = http.createServer();
+const io = new Server(server);
 
-const io = new Server(server)
-
-io.on('connection', (socket) => {
-  console.log('user connected')
-  console.log(socket)
-})
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log(socket);
+});
 
 server.listen(4000, () => {
-  console.log('listening on 4000')
-})
+  console.log("listening on 4000");
+});
