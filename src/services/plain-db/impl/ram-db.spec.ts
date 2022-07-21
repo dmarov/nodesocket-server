31200801994ce732e0bb52211930f9a2464a4279
Dmitry Marov <d.marov94@gmail.com>
2022-07-21 19:06:05 +0300

    unit tests wip

---
+++ src/services/plain-db/impl/ram-db.spec.ts
@@ -0,0 +1,19 @@
+// import { RamDb } from "./ram-db";
+
+describe('RamDb tests', () => {
+  it('checks if entry is added to db', () => {
+    // const db = new RamDb();
+    // const key = 'test-key';
+    // db.add(key, "value");
+    // const result = db.add(key, "value");
+
+    expect(2).toEqual(2);
+    // const value = result.unwrap(
+    //   value => value,
+    //   () => {
+    //     throw new Error();
+    //   },
+    // );
+
+  })
+});
