6f0bf9c6421f0136fd17a612e3b2b14ce16364a7
Dmitry Marov <d.marov94@gmail.com>
2022-07-23 16:47:10 +0300

    added jest

--- src/services/plain-db/impl/ram-db.spec.ts
+++ src/services/plain-db/impl/ram-db.spec.ts
@@ -1,13 +1,13 @@
 // import { RamDb } from "./ram-db";
 
-describe("RamDb tests", () => {
-  it("checks if entry is added to db", () => {
-    // const db = new RamDb();
-    // const key = "test-key";
-    // const result = db.add(key, "value");
-    // const isSuccess = result.unwrap(() => true, () => false);
-
-    // expect(isSuccess).toBeTrue();
-    expect(true).toBeTrue();
-  });
+test("checks if entry is added to db", () => {
+  expect(1).toBe(1);
 });
+
+// const db = new RamDb();
+// dejk
+// const key = "test-key";
+// const result = db.add(key, "value");
+// const isSuccess = result.unwrap(() => true, () => false);
+
+// expect(isSuccess).toBeTrue();
