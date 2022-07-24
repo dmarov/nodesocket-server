1245f4447ea3bfe018f130c4e941c3f22e0f1e81
Dmitry Marov <d.marov94@gmail.com>
2022-07-24 19:42:18 +0300

    added test

--- src/services/user-identity-handler/impl/user-identity-handler.spec.ts
+++ src/services/user-identity-handler/impl/user-identity-handler.spec.ts
@@ -1,3 +1,4 @@
+import { ErrorCodes } from "@/errors";
 import { RamDb } from "@/services/plain-db";
 import { UserIdentityPersistenceService } from "@/services/user-identity-persistence";
 import { UserIdentityValidationService } from "@/services/user-identity-validation";
@@ -15,4 +16,14 @@ test("user identity accepted", () => {
   const service = factory();
   const result = service.addIdentity({name: "John Doe"});
   expect(result.checkSuccess()).toBe(true);
+});
+
+test("user identity accepted", () => {
+  const service = factory();
+  service.addIdentity({name: "John Doe"});
+  service.addIdentity({name: "John Doe 1"});
+  service.addIdentity({name: "John Doe 2"});
+  const result = service.addIdentity({name: "John Doe 3"});
+  const isSuccess = result.unwrap(() => false, e => e.id === ErrorCodes.LimitExceeded);
+  expect(isSuccess).toBe(true);
 });
